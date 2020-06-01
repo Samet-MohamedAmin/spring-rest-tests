d088c4d8ef30369c1ee48d58be4b81d34078f1f9
mohamed.samet <mohamed.samet@kshuttle.io>
2020-06-01 23:50:41 +0200

    remove everything

--- data/src/main/java/com/assignment/recipe/models/BaseEntity.java
+++
@@ -1,8 +0,0 @@
-package com.assignment.recipe.models;
-
-public interface BaseEntity {
-
-	public Long getId();
-	
-	public void setId(Long id);
-}
--- data/src/main/java/com/assignment/recipe/services/PotatoService.java
+++
@@ -1,9 +0,0 @@
-package com.assignment.recipe.services;
-
-import com.assignment.recipe.models.Potato;
-
-
-public interface PotatoService extends CrudService<Potato, Long> {
-
-}
-
